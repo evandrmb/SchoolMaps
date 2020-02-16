65a1489b488d1747b04221dcdc35d87807f055e0
Evandro Junior <47798920+evandrmb@users.noreply.github.com>
2020-02-16 20:08:02 -0300

    PolyLines added
    Just a line traced between user current location and one of the schools (since i wait some time to get the user current location, because it's a async method i save the user last known location and drawn a line between that and the location, while i wait for the current location)

--- lib/map.dart
+++ lib/map.dart
@@ -9,6 +9,38 @@ class MyMap extends StatefulWidget {
 }
 
 class _MyMapState extends State<MyMap> {
+  Map<PolylineId, Polyline> _mapPolylines = {};
+  int _polylineIdCounter = 1;
+  Position myLastPosition;
+
+  List<LatLng> _createPoints(Position currentLocation, LatLng coords) {
+    final List<LatLng> points = <LatLng>[
+      LatLng(currentLocation.latitude, currentLocation.longitude),
+      coords
+    ];
+
+    return points;
+  }
+
+  void _add(Position currentLocation, LatLng coords) {
+    final String polylineIdVal = 'polyline_id_$_polylineIdCounter';
+    _polylineIdCounter++;
+    final PolylineId polylineId = PolylineId(polylineIdVal);
+
+    final Polyline polyline = Polyline(
+      polylineId: polylineId,
+      consumeTapEvents: true,
+      color: Colors.red[200],
+      width: 4,
+      points: _createPoints(currentLocation, coords),
+    );
+
+    setState(() {
+      _mapPolylines.clear();
+      _mapPolylines[polylineId] = polyline;
+    });
+  }
+
   GoogleMapController _controller;
 
   List<Marker> allMarkers = [];
@@ -18,6 +50,13 @@ class _MyMapState extends State<MyMap> {
     theschools.map((sch) {
       allMarkers.add(
         Marker(
+          onTap: () async {
+            if (myLastPosition != null) _add(myLastPosition, sch.coords);
+            var currentlocation = await Geolocator()
+                .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+            myLastPosition = currentlocation;
+            _add(currentlocation, sch.coords);
+          },
           markerId: MarkerId(sch.name),
           draggable: false,
           infoWindow: InfoWindow(title: sch.name, snippet: sch.adress),
@@ -27,29 +66,29 @@ class _MyMapState extends State<MyMap> {
     }).toList();
   }
 
-  void _getLocation() async {
-    var currentLocation = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+  // void _getLocation() async {
+  //   var currentLocation = await Geolocator()
+  //       .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
 
-    setState(() {
-      final marker = Marker(
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-        markerId: MarkerId("user_location"),
-        position: LatLng(currentLocation.latitude, currentLocation.longitude),
-        infoWindow: InfoWindow(title: 'Your Location'),
-      );
-      allMarkers.add(marker);
-    });
-  }
+  //   setState(() {
+  //     final marker = Marker(
+  //       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+  //       markerId: MarkerId("user_location"),
+  //       position: LatLng(currentLocation.latitude, currentLocation.longitude),
+  //       infoWindow: InfoWindow(title: 'You was here'),
+  //     );
+  //     allMarkers.add(marker);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getLocation,
-        tooltip: 'Get Location',
-        child: Icon(Icons.flag),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _getLocation,
+      //   tooltip: 'Get Location',
+      //   child: Icon(Icons.flag),
+      // ),
       appBar: AppBar(
         title: Text('School Routes'),
         centerTitle: true,
@@ -60,6 +99,8 @@ class _MyMapState extends State<MyMap> {
             height: MediaQuery.of(context).size.height - 50,
             width: double.infinity,
             child: GoogleMap(
+              polylines: Set<Polyline>.of(_mapPolylines.values),
+              myLocationEnabled: true,
               markers: Set.from(allMarkers),
               onMapCreated: mapCreated,
               initialCameraPosition: CameraPosition(
